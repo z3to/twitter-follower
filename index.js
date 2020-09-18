a74f075d1fd015686436a8d63b6d14524fb1e6cb
z3to <6gqrbj@posteo.org>
2020-09-18 13:14:05 +0200

    Init

---
+++ index.js
@@ -0,0 +1,95 @@
+require('dotenv').config()
+
+const { get, without, forEach, throttle, size, find, isUndefined } = require('lodash')
+const { authRequest, getList, getUserInfo, logToGoogleForm } = require('./requests.js')
+const CronJob = require('cron').CronJob
+
+const { format, parse } = require('date-fns')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('user.json')
+const db = low(adapter)
+
+db.defaults({ currentUser: [], changes: [] })
+  .write()
+
+const USERNAME = process.env.USERNAME
+const PASSWORD = process.env.PASSWORD
+const USER_ID = process.env.USER_ID
+
+let BEARER
+
+function timestamp () {
+	return format(new Date(), 'HH:mm:ss dd-MM-yyyy')
+}
+
+function formatDate (date) {
+	return format(date, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")
+}
+
+function parseDate (date) {
+	return parse(date, 'EEE MMM dd kk:mm:ss xxxx yyyy', new Date())
+}
+
+function compareLists (newList, oldList) {
+	const left = without(oldList, ...newList)
+	const join = without(newList, ...oldList)
+
+	const user = {}
+	forEach(left, id => {
+		user[id] = 'left'
+	})
+	forEach(join, id => {
+		user[id] = 'join'
+	})
+	return user
+}
+
+function processList (newUser) {
+	const oldUser = db.get('currentUser').value()
+
+	const changes = compareLists(newUser, oldUser)
+
+	db.set('currentUser', newUser).write()
+
+	console.log(`${timestamp()}: ${newUser.length} user in total. ${size(changes)} changes.`)
+
+	if (size(changes)) {
+		getUserInfo(changes, addChanges)
+	}
+}
+
+function addChanges (changes, details) {
+	forEach(changes, (action, id) => {
+		const user = find(details, { id_str: id })
+
+  	const detail = {
+  		action,
+  		id,
+  		name: get(user, 'name'),
+	  	screen_name: get(user, 'screen_name'),
+	  	url: get(user, 'url'),
+	  	followers_count: get(user, 'followers_count'),
+	  	friends_count: get(user, 'friends_count'),
+	  	listed_count: get(user, 'listed_count'),
+	  	created_at: formatDate(parseDate(get(user, 'created_at'))),
+	  	statuses_count: get(user, 'statuses_count'),
+	  	following: get(user, 'following'),
+	  	verified: get(user, 'verified'),
+	  	location: get(user, 'location'),
+	  	description: get(user, 'description'),
+	  	timestamp: formatDate(new Date())
+  	}
+
+  	console.log(`${timestamp()}: ${detail.screen_name || 'unknown'}: ${action}`)
+  	db.get('changes').push(detail).write()
+  	logToGoogleForm(detail)
+	})
+}
+
+new CronJob('0 * * * *', function() {
+	authRequest(() => {
+		getList(processList)
+	})
+}, null, true);
---
+++ requests.js
@@ -0,0 +1,80 @@
+const request = require('request')
+const { get, keys, forEach } = require('lodash')
+
+const USERNAME = process.env.USERNAME
+const PASSWORD = process.env.PASSWORD
+const USER_ID = process.env.USER_ID
+
+let BEARER
+
+module.exports = {
+  authRequest (callback) {
+  	// console.log(`Starting authRequest`)
+    request.post(`https://api.twitter.com/oauth2/token?grant_type=client_credentials`, {
+	    auth: {
+	      user: USERNAME,
+	      pass: PASSWORD
+	    }
+	  },
+	  (error, response, body) => {
+	  	BEARER = get(JSON.parse(body), 'access_token')
+	  	// console.log(`Finishing authRequest`)
+	  	callback()
+	  })
+  },
+  getList (callback) {
+  	// console.log(`Starting getList`)
+    request.get(`https://api.twitter.com/1.1/followers/ids.json?user_id=${USER_ID}&stringify_ids=true`, {
+	    auth: {
+	      bearer: BEARER
+	    }
+	  },
+	  (error, response, body) => {
+	  	// console.log(`Finishing getList`)
+	  	callback(get(JSON.parse(body), 'ids', []))
+	  })
+  },
+  getUserInfo (changes, callback) {
+  	// console.log(`Starting getUserInfo`)
+  	const ids = keys(changes).join(',')
+    request.get(`https://api.twitter.com/1.1/users/lookup.json?user_id=${ids}`, {
+	    auth: {
+	      bearer: BEARER
+	    }
+	  },
+	  (error, response, body) => {
+	  	// console.log(`Finishing getUserInfo`)
+	  	callback(changes, JSON.parse(body))
+	  })
+  },
+  logToGoogleForm (detail) {
+  	let entries = {
+  		'entry.1853612370': 'action',
+  		'entry.768425125': 'name',
+  		'entry.1854247375': 'screen_name',
+  		'entry.621019786': 'url',
+  		'entry.1612288882': 'followers_count',
+  		'entry.1754222331': 'friends_count',
+  		'entry.159042684': 'listed_count',
+  		// 'entry.1978602096': 'created_at',
+  		'entry.1713027614': 'statuses_count',
+  		'entry.1916892429': 'following',
+  		'entry.935811465': 'verified',
+  		'entry.1500016717': 'location',
+  		// 'entry.327424402': 'description',
+  		'entry.473828998': 'id'
+  	}
+  	forEach(entries, (entry, key) => {
+  		entries[key] = get(detail, entry, '') || ''
+  	})
+  	// console.log(`Starting logToGoogleForm`)
+    request.post({
+	    	url: `https://docs.google.com/forms/u/0/d/e/1FAIpQLSfjGnT7gC2bI1OU1jwDP9HjDDsQLTDgtZeM7bJzJ27JzhbknQ/formResponse`,
+	    	form: entries
+    	},
+		  (error, response, body) => {
+		  	// console.log(`Finishing logToGoogleForm`)
+		  }
+	  )
+  }
+};
